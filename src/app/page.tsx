0fafc3cca0c0959223d7e50d3a2a7e4df463caf9
Nikhil Patel <nikhil6.patel@ril.com>
2024-04-14 18:52:26 +0530

    Escaping '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between mt-24 sm:mt-40">
       <div className="text-center space-y-4">
         <h1 className="font-extrabold text-4xl leading-9 sm:text-5xl sm:leading-10 md:text-5xl md:leading-14">
-          Hi! I'm <span className="text-primary">Nikhil</span>
+          Hi! I&apos;m <span className="text-primary">Nikhil</span>
         </h1>
         <h2 className=" font-bold text-2xl leading-9 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
-          I'm a software developer
+          I&apos;m a software developer
         </h2>
         <p className="text-sm leading-7 sm:text-base sm:leading-10 md:text-lg text-secondary-foreground">
           Checkout my <CustomLink link={RESUME_LINK}>Resume</CustomLink> or <CustomLink link={CONTACT_ME_LINK}>Contact me</CustomLink>
