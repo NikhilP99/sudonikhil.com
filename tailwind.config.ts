89b0e2bc7564f876600e157b4d20e0e1eb437589
Nikhil Patel <nikhilpatel.indore@gmail.com>
2025-02-19 11:39:53 +0530

    Adding fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+        mono: ['var(--font-roboto-mono)'],
       },
     },
   },
